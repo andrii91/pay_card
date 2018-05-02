8a157114e644d03b696d7952439bf1028dfbf780
Andrii Litvin <al@gogmat.com>
2018-05-02 13:38:11 +0300

    fix

--- src/js/component.js
+++ src/js/component.js
@@ -66,7 +66,6 @@ $(document).ready(function () {
     $('.'+$(this).attr('name')).text($(this).val());
   });
   
-    /*db/registration.php*/
 
   /* form valid*/
   var alertImage = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 286.1 286.1"><path d="M143 0C64 0 0 64 0 143c0 79 64 143 143 143 79 0 143-64 143-143C286.1 64 222 0 143 0zM143 259.2c-64.2 0-116.2-52-116.2-116.2S78.8 26.8 143 26.8s116.2 52 116.2 116.2S207.2 259.2 143 259.2zM143 62.7c-10.2 0-18 5.3-18 14v79.2c0 8.6 7.8 14 18 14 10 0 18-5.6 18-14V76.7C161 68.3 153 62.7 143 62.7zM143 187.7c-9.8 0-17.9 8-17.9 17.9 0 9.8 8 17.8 17.9 17.8s17.8-8 17.8-17.8C160.9 195.7 152.9 187.7 143 187.7z" fill="#E2574C"/></svg>';
@@ -154,6 +153,18 @@ $(document).ready(function () {
   
   $('select').change(function(){
     $('.'+$(this).attr('name')).text($(this).val());
+    $('.delivery_summ').text('$'+$(this).find('option[value="'+$(this).val()+'"]').data('price'));
+    
+    $('select option[value="'+$(this).val()+'"]').attr('selected', 'selected');
+    
+    
+    var price = parseInt($('.product-price').text());
+    var delivery_summ = parseInt($(this).find('option[value="'+$(this).val()+'"]').data('price'));
+    var quantity = parseInt($('#quantity').val());;
+    $('.quantity_count').text(quantity);
+
+    $('.subtotal_summ').text(price*quantity + "$");
+    $('.summ_all').text(price*quantity + delivery_summ + "$");
     
     $('body').removeClass('overl-h');
     $('.modal').hide('400');
